5b63f95931490f76723d8d96797580b515506eb1
jakubczerwonski <jakubczerwonski@gmail.com>
2023-05-18 12:48:18 +0200

    encoding correction

--- my-app-gui.py
+++ my-app-gui.py
@@ -2,12 +2,13 @@ import streamlit as st
 import pandas as pd
 import io
 import os
+import codecs
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 
-def create_agent(api_key, csv_data, temperature=0):
+def create_agent(api_key, csv_data, temperature=0, encoding="utf-8"):
     os.environ["OPENAI_API_KEY"] = api_key
-    return create_csv_agent(OpenAI(temperature=temperature), csv_data, verbose=True)
+    return create_csv_agent(OpenAI(temperature=temperature), csv_data, verbose=True, encoding=encoding)
 
 def run_agent(agent, query):
     if agent is not None:
@@ -34,8 +35,15 @@ def main():
 
     agent = None
     if openai_api_key and uploaded_file is not None:
-        df = pd.read_csv(uploaded_file, encoding="latin-1")
         csv_data = io.StringIO()
+        if uploaded_file.name.endswith('.csv'):
+            try:
+                df = pd.read_csv(uploaded_file, encoding="utf-8")
+            except UnicodeDecodeError:
+                uploaded_file.seek(0)
+                df = pd.read_csv(codecs.iterdecode(uploaded_file, 'utf-8'), encoding="utf-8")
+        else:
+            st.write("File type not supported. Please upload a CSV file.")
         df.to_csv(csv_data, index=False)
         csv_data.seek(0)
         agent = create_agent(openai_api_key, csv_data)
